513567271d97390ca6d03220df5022dab91eb4cd
Magnus Ottosen <magnus@ottosen.dk>
2024-03-08 08:21:44 +0100

    Change to http

--- server/server.js
+++ server/server.js
@@ -1,30 +1,19 @@
 //This is where the server is
 
-const fs = require("fs"); //Node.js file system
-const https = require("https");
+const http = require("http");
 const express = require("express");
 const socketIo = require("socket.io");
 
 const app = express();
 
-//SSL certification
-const options = {
-  key: fs.readFileSync("./ssl/private.key"),
-  cert: fs.readFileSync("./ssl/certificate.crt")
-};
-
-//Create HTTPS server
-const server = https.createServer(options, app);
+const server = http.createServer(app);
 const io = socketIo(server);
 
-//Sends whats in our client to the client.
 app.use(express.static("client"));
 
-//WebSocket connections
 io.on("connection", (socket) => {
     console.log("A user connected");
 
-    //Listen for a client sending something to server. If something is sendt, send something to all clients.
     socket.on("send-message", (message) => {
         io.emit("receive-message", message);
     });
