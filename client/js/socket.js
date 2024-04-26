dc3821791e426f7046361f899c9049baa33d21b1
Magnus Ottosen <magnus@ottosen.dk>
2024-04-26 08:25:00 +0200

    fixed typo

--- client/js/socket.js
+++ client/js/socket.js
@@ -31,7 +31,7 @@ export function emitDrop(){
 }
 
 //Listen for game state updates from the server
-socket.on('gameState', (gameState) => {
+socket.on('game-state', (gameState) => {
     console.log("Received gamestate from server: ", gameState);
 
 
