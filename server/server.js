f7296a8012dc8e4d6963d0860bc69be6addf6d22
Camellia <91070768+s203237@users.noreply.github.com>
2024-05-21 09:45:22 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 //This is where the server is
 import http from 'http';
 import express from 'express';
-import { Server } from 'socket.io';
+import {Server} from 'socket.io';
 import path from 'path';
-import { RoomManager } from './controllers/roomManager.js';
-import { fileURLToPath } from 'url';
-
-
-// const http = require("http");
-// const expraess = require("express");
-// const socketIo = require("socket.io");
+import {RoomManager} from './controllers/roomManager.js';
+import {fileURLToPath} from 'url';
 // const path = require('path');
 
 const app = express();
@@ -21,6 +16,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://dtu62597.eduhost.dk:10311",  // Allows all domains, adjust as needed for security
+        // origin: "http://localost:3000",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
@@ -33,14 +29,14 @@ const clientPath = path.join(__dirname, '..', 'client');
 app.use(express.static(clientPath));
 
 const roomManager = new RoomManager(io);
-for (let i = 1; i <= 4 ; i++) {
+for (let i = 1; i <= 4; i++) {
     roomManager.createRoom("room" + i);
 }
 
 io.on("connection", (socket) => {
     // console.log("A user connected");
     console.log(socket.connected)
-    
+
     socket.on("send-message", (message) => {
     });
     socket.on("init-game", (message) => {
@@ -48,7 +44,7 @@ io.on("connection", (socket) => {
         io.emit("init-game", message);
         let myArray = message.split("|");
         let name = myArray[0];
-        let roomId = myArray[1]; 
+        let roomId = myArray[1];
         roomManager.getRoom(roomId).addPlayer(socket.id, name);
     });
 
@@ -66,7 +62,7 @@ io.on("connection", (socket) => {
     socket.on("playerRotate", (rotationDirection) => {
         roomManager.getGameController(socket.id).handlePlayerRotation(socket.id, rotationDirection);
 
-        });
+    });
 
     socket.on("playerFall", () => {
         roomManager.getGameController(socket.id).handlePlayerFall(socket.id);
@@ -77,7 +73,6 @@ io.on("connection", (socket) => {
     });
 
     socket.on("playerDrop", () => {
-
         roomManager.getGameController(socket.id).handleDrop(socket.id);
 
     });
@@ -86,19 +81,19 @@ io.on("connection", (socket) => {
         console.log('testing');
         io.emit("receive-message", "testing ack");
     });
-    socket.on('ready',(roomId) => {
+    socket.on('ready', (roomId) => {
         roomManager.getGameController(socket.id).playerReady(socket.id);
     });
-    socket.on("game-board",(gameBoard) => {
+    socket.on("game-board", (gameBoard) => {
         let controller = roomManager.getGameController(socket.id);
-        if(controller!=null) {
-            controller.updateGameBoard(socket.id, gameBoard);
+        if (controller != null) {
+            controller.updateGameBoards(gameBoard, socket.id);
         }
     });
     socket.on("game-over", (player) => {
         let controller = roomManager.getGameController(socket.id);
-        if(controller!=null) {
-            controller.handleGameOver(socket.id, player);
+        if (controller != null) {
+            controller.updateGameOver(socket.id, player);
         }
     });
 });
